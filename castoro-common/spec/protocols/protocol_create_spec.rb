cd2c9883f3f8662b9d3c1701b3115843b422e7bf
matsushita <matsushita@lassic.co.jp>
2011-12-21 14:38:53 +0900

    Added CREATE spec with island
    * issue #52

--- castoro-common/spec/protocols/protocol_create_spec.rb
+++ castoro-common/spec/protocols/protocol_create_spec.rb
@@ -32,12 +32,8 @@ describe Castoro::Protocol::Command::Create do
     }.should raise_error(RuntimeError, "Nil cannot be set for class.")
   end
 
-  context 'when initialize, argument for basket set "1.2.3", argument for hints set {"length" => "12345", "class" => 1}' do
-    it "should be able to create an instance of create command." do
-      Castoro::Protocol::Command::Create.new("1.2.3", {"length" => "12345", "class" => 1}).should be_kind_of(Castoro::Protocol::Command::Create)
-    end
-
-    context "when initialized" do
+  describe "#initialize" do
+    context "given basket." do
       before do
         @command = Castoro::Protocol::Command::Create.new "1.2.3", {"length" => "12345", "class" => 1}
       end
@@ -79,16 +75,50 @@ describe Castoro::Protocol::Command::Create do
         JSON.parse(error_res.to_s).should == JSON.parse('["1.1","R","CREATE",{"basket":null,"hosts":null,"error":"Unexpected error!"}]' + "\r\n")
       end
     end
+
+    context "given basket and island." do
+      before do
+        @command = Castoro::Protocol::Command::Create.new "1.2.3", {"length" => "12345", "class" => 1}, "island" => "abc45678"
+      end
+
+      it "basket is '1.2.3'." do
+        basket = @command.basket
+        basket.should be_kind_of(Castoro::BasketKey)
+        basket.to_s.should == "1.2.3"
+      end
+
+      it "island is 'abc45678'." do
+        island = @command.island
+        island.should == "abc45678"
+      end
+
+      it "should be able to get :hints." do
+        @command.hints.should == {"length"=>12345, "class"=>"1"}
+      end
+
+      it "should be able to get hints#klass." do
+        @command.hints.klass.should == "1"
+      end
+
+      it "should be able to get hints#length." do
+        @command.hints.length.should == 12345
+      end
+
+      it "should be able to use #to_s." do
+        JSON.parse(@command.to_s).should ==
+          JSON.parse('["1.1","C","CREATE",{"basket":"1.2.3","hints":{"length":12345,"class":"1"},"island":"abc45678"}]' + "\r\n")
+      end
+    end
   end
 end
 
 describe Castoro::Protocol::Response::Create do
-  context 'when initialize, argument for error set nil, argument for basket set to "1.2.3"' do
-    it "should be able to create an instance of create response." do
-      Castoro::Protocol::Response::Create.new(nil, "1.2.3").should be_kind_of(Castoro::Protocol::Response::Create)
-    end
+  it "should be able to create an instance of create response." do
+    Castoro::Protocol::Response::Create.new(nil, "1.2.3").should be_kind_of(Castoro::Protocol::Response::Create)
+  end
 
-    context "when initialized" do
+  describe "#initialize" do
+    context "given basket" do
       before do
         @response = Castoro::Protocol::Response::Create.new nil, "1.2.3"
       end
@@ -106,6 +136,32 @@ describe Castoro::Protocol::Response::Create do
       it 'should be able to use #to_s.' do
         JSON.parse(@response.to_s).should ==
           JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3"}]' + "\r\n")
+      end
+    end
+
+    context "given basket and island" do
+      before do
+        @response = Castoro::Protocol::Response::Create.new nil, "1.2.3", "abc45678"
+      end
+
+      it 'basket is "1.2.3".' do
+        basket = @response.basket
+        basket.should be_kind_of(Castoro::BasketKey)
+        basket.to_s.should == "1.2.3"
+      end
+
+      it 'island is "abc45678".' do
+        island = @response.island
+        islnad.should == "abc45678"
+      end
+
+      it 'should be #error? false.' do
+        @response.error?.should be_false
+      end
+
+      it 'should be able to use #to_s.' do
+        JSON.parse(@response.to_s).should ==
+          JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3","island":"abc45678"}]' + "\r\n")
       end
     end
   end
@@ -143,12 +199,8 @@ describe Castoro::Protocol::Response::Create::Gateway do
     }.should raise_error(RuntimeError, "Nil cannot be set for hosts.")
   end
 
-  context 'when initialize, argument for basket set "1.2.3", argument for hosts set [ "host100", "host101", "host102" ] ' do
-    it 'should be able to create an instance of create gateway response.' do
-      Castoro::Protocol::Response::Create::Gateway.new(nil, "1.2.3", [ "host100", "host101", "host102" ]).should be_kind_of(Castoro::Protocol::Response::Create::Gateway)
-    end
-
-    context "when initialized" do
+  decribe "#initialize" do
+    context "given basket and hosts." do
       before do
         @response = Castoro::Protocol::Response::Create::Gateway.new nil, "1.2.3", [ "host100", "host101", "host102" ]
       end
@@ -187,6 +239,53 @@ describe Castoro::Protocol::Response::Create::Gateway do
       it 'should be able to use #to_s.' do
         JSON.parse(@response.to_s).should ==
           JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3","hosts":["host100","host101","host102"]}]' + "\r\n")
+      end
+    end
+
+    context "given basket, hosts and island." do
+      before do
+        @response = Castoro::Protocol::Response::Create::Gateway.new nil, "1.2.3", [ "host100", "host101", "host102" ], "abc45678"
+      end
+
+      it 'basket is "1.2.3".' do
+        basket = @response.basket
+        basket.should be_kind_of(Castoro::BasketKey)
+        basket.to_s.should == "1.2.3"
+      end
+
+      it 'island is "abc45678".' do
+        island = @response.island
+        islnad.should == "abc45678"
+      end
+
+      it 'hosts is "host100", "host101", "host102".' do
+        @response.hosts.should == [ "host100", "host101", "host102" ]
+      end
+
+      it 'should be able to use #each(&block).' do
+        hosts = []
+        @response.each{|host|
+          hosts << host
+        }
+        hosts.should == [ "host100", "host101", "host102" ]
+      end
+
+      it 'should be able to use #[](index).' do
+        @response[0].should == "host100"
+        @response[1].should == "host101"
+        @response[2].should == "host102"
+        @response.[](0).should == "host100"
+        @response.[](1).should == "host101"
+        @response.[](2).should == "host102"
+      end
+
+      it 'should be #error? false.' do
+        @response.error?.should be_false
+      end
+
+      it 'should be able to use #to_s.' do
+        JSON.parse(@response.to_s).should ==
+          JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3","hosts":["host100","host101","host102"],"island":"abc45678"}]' + "\r\n")
       end
     end
   end
@@ -234,12 +333,12 @@ describe Castoro::Protocol::Response::Create::Peer do
     }.should raise_error(RuntimeError, "Nil cannot be set for path.")
   end
 
-  context 'when initialize, argument for error set nil, argument for basket set "1.2.3", argument for host set "host100", argument for path set "/path/1.2.3"' do
+  describe "#initialize" do
     it 'should be able to create an instance of create peer response.' do
       Castoro::Protocol::Response::Create::Peer.new(nil, "1.2.3", "host100", "/path/1.2.3").should be_kind_of(Castoro::Protocol::Response::Create::Peer)
     end
 
-    context 'when initialized' do
+    context 'given basket, host and path.' do
       before do
         @response = Castoro::Protocol::Response::Create::Peer.new nil, "1.2.3", "host100", "/path/1.2.3"
       end
@@ -265,6 +364,40 @@ describe Castoro::Protocol::Response::Create::Peer do
       it 'should be able to use #to_s.' do
         JSON.parse(@response.to_s).should ==
           JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3","host":"host100","path":"/path/1.2.3"}]' + "\r\n")
+      end
+    end
+
+    context 'given basket, host, path and island.' do
+      before do
+        @response = Castoro::Protocol::Response::Create::Peer.new nil, "1.2.3", "host100", "/path/1.2.3", "abc45678"
+      end
+
+      it 'should be #error? false.' do
+        @response.error?.should be_false
+      end
+
+      it 'should be able to get :basket.' do
+        basket = @response.basket
+        basket.should be_kind_of(Castoro::BasketKey)
+        basket.to_s.should == "1.2.3"
+      end
+
+      it 'island is "abc45678".' do
+        island = @response.island
+        islnad.should == "abc45678"
+      end
+
+      it 'should be able to get :host.' do
+        @response.host.should == "host100"
+      end
+
+      it 'should be able to get :path.' do
+        @response.path.should == "/path/1.2.3"
+      end
+
+      it 'should be able to use #to_s.' do
+        JSON.parse(@response.to_s).should ==
+          JSON.parse('["1.1","R","CREATE",{"basket":"1.2.3","host":"host100","path":"/path/1.2.3","island":"abc45678"}]' + "\r\n")
       end
     end
   end
