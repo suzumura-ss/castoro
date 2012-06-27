737b85f15a8ade352e0696b06ea66e1484408368
dnaka <daisuke.nakagawa@jrits.ricoh.co.jp>
2012-06-27 16:37:18 +0900

    Change the version number from 0.1.1 to 1.1.0

--- castoro-manipulator/lib/castoro-manipulator/version.rb
+++ castoro-manipulator/lib/castoro-manipulator/version.rb
@@ -3,9 +3,9 @@ module Castoro #:nodoc:
   module Manipulator #:nodoc:
     module Version #:nodoc:
       unless defined? MAJOR
-        MAJOR  = 0
+        MAJOR  = 1
         MINOR  = 1
-        TINY   = 1
+        TINY   = 0
         PRE    = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
